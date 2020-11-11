b4c8b48f08bd7ddb4cc8536b9655ed70f00b6b40
Rob Sweny <Robsweny@gmail.com>
2020-11-11 20:08:58 +0000

    first commit

---
+++ ChessGameModel/Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGameModel
+{
+    class Board
+    {
+        // the Size of the board
+        public int Size { get; set; }
+
+        // 2D array of the type cell
+        public Cell[,] theGrid { get; set; }
+
+        public Board (int s)
+        {   
+            // initial size of the board is defined by s
+            Size = s;
+
+            // create a new 2D array of type cell
+            theGrid = new Cell[Size, Size];
+
+            // fill the 2d array with new Cells, each with a unique X and Y property (Row and Column)
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves ( Cell currentCell, string chessPeice)
+        {
+            // clear all previous moves
+
+
+            // find all legal moves and mark the cells
+
+        }
+    }
+}
---
+++ ChessGameModel/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGameModel
+{
+    class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool CurrentlyOccupied { get; set; }
+        public bool LegalNextMove { get; set; }
+
+        public Cell(int row, int column)
+        {
+            RowNumber = row;
+            ColumnNumber = column;
+        }
+    }
+}
